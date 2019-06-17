64afc5ca673a0b4b25e59fc48d94dab7f8d8a88c
zhanyusha <1875931641@qq.com>
2019-06-17 11:11:49 +0800

    add bootstrap

--- src/main/java/com/john/community/community/controller/IndexController.java
+++ src/main/java/com/john/community/community/controller/IndexController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by codedrinker on2019/6/17
  */
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
